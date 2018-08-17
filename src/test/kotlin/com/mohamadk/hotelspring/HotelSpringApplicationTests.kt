1cabde2bb596e9c3042739eba1663ae43e42c7a1
mohamad <mohamad.khaleghy@gmail.com>
2018-08-17 09:34:16 +0430

    Genesis commit

---
+++ src/test/kotlin/com/mohamadk/hotelspring/HotelSpringApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.mohamadk.hotelspring
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class HotelSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
